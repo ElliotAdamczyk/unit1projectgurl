8c86eb4ee971fcfc96d4f86f56812554079d80fc
Elliot Adamczyk <Ad0773El0811@pusd.us>
2023-08-30 10:48:32 -0700

    beginning of code

--- u1proj.py
+++ u1proj.py
@@ -1,4 +1,13 @@
+userPick = []
 
-x = "hello"
+def userSelection():
+    u1 = input("Player one do you want to play Rock(r), Paper(p), or Scizzors(s)").append(userPick)
+    u2 = input("Player two do you want to play Rock(r), Paper(p), or Scizzors(s)").append(userPick)
+    
+print (userSelection())
 
-print(x)
+def playGame(u1, u2):
+    print(hi)
+
+
+
