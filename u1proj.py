ee59454702b71e6594d7ac9e1dc90adff23df29b
ElliotAdamczyk <142929718+ElliotAdamczyk@users.noreply.github.com>
2023-08-31 10:42:11 -0700

    Update u1proj.py

--- u1proj.py
+++ u1proj.py
@@ -1,13 +1,11 @@
 userPick = []
 
 def userSelection():
-    u1 = input("Player one do you want to play Rock(r), Paper(p), or Scizzors(s)").append(userPick)
-    u2 = input("Player two do you want to play Rock(r), Paper(p), or Scizzors(s)").append(userPick)
-    
+    while userPick <= 2:
+        u1 = input("Player one do you want to play Rock(r), Paper(p), or Scizzors(s)")
+        u2 = input("Player two do you want to play Rock(r), Paper(p), or Scizzors(s)")
+        if u1 == "r" or u1 == "s" or u1 == "p":
+            userPick.append(u1)
+        
 print (userSelection())
-
-def playGame(u1, u2):
-    print(hi)
-
-
-
+print (userPick)
